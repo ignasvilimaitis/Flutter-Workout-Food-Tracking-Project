f4b8b175b62a8f60d5c13fb1736857b0178b09b3
Ignas Vilimatis <iv222en@student.lnu.se>
2025-09-14 19:39:37 +0200

    more work on food selection UI

--- lib/features/food-logging/food_selection.dart
+++ lib/features/food-logging/food_selection.dart
@@ -18,95 +18,140 @@ class _FoodSelectorState extends State<FoodSelector> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: Padding(
-          padding: const EdgeInsets.all(6.0),
-          child: Container(
-            width: 45,
-            height: 45,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular((16.0)),
-              color: Colors.white,
-            ),
-            child: IconButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              icon: Icon(Icons.close),
-            ),
+      body: Column( // TODO: Change so the header is not part of the scrollable list but also not fixed to the top (disappears when scrolling down)
+        children: [
+          Padding(padding: const EdgeInsets.only(top: 20.0, left: 16.0, right: 16.0,),
           ),
-        ),
-        backgroundColor: Theme.of(context).primaryColor,
-        title: Container(
-          width: 260,
-          height: 45,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular((16.0)),
-            color: Colors.white,
-          ),
-          child: TextField(
-            decoration: InputDecoration(
-              hintText: 'Search foods',
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(16.0),
-                borderSide: BorderSide.none
+          // Header
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(6.0),
+                child: Container(
+                  width: 45,
+                  height: 45,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular((16.0)),
+                    color: Colors.white,
+                  ),
+                  child: IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: Icon(Icons.close),
+                  ),
+                ),
               ),
-            ),
-            onSubmitted: (String value) async {
-              print('Searching for: $value');
-              SearchResult? result = await query.searchProducts(value);
-              List<Product>? products = result?.products;
+              Container(
+                width: 295,
+                height: 45,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular((16.0)),
+                  color: Colors.white,
+                ),
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    const Icon(Icons.search),
+                    const SizedBox(width: 8.0),
+                    SizedBox(
+                      width: 210,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.symmetric(vertical: 6.0, horizontal: 10.0),
+                          hintText: 'Search all food...',
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                            borderRadius: BorderRadius.circular(16.0),
 
-              setState(() {
-                foods =
-                    products?.map((product) {
-                      final nutr = product.nutriments;
-
-                      // helper to read most nutrients per 100g
-                      double _get(Nutrient n) =>
-                          nutr?.getValue(n, PerSize.oneHundredGrams) ?? 0.0;
-                      // Gotta use helper methods for all the macros since they removed direct getters, e.g. product.carbohydrates
-                      // Now you have to read through enum values
-
-                      // Calories: prefer kcal; if not available, try kJ -> convert to kcal (1 kcal = 4.184 kJ)
-                      double _getCalories() {
-                        final kcal = nutr?.getValue(
-                          Nutrient.energyKCal,
-                          PerSize.oneHundredGrams,
-                        );
-                        if (kcal != null) return kcal;
-                        final kj = nutr?.getComputedKJ(PerSize.oneHundredGrams);
-                        if (kj != null) return kj / 4.184;
-                        return 0.0;
-                      }
-
-                      return FoodItem(
-                        brand: product.brands ?? 'Unknown Brand',
-                        productName: product.productName ?? 'Unknown Product',
-                        calories: _getCalories(),
-                        carbs: _get(Nutrient.carbohydrates),
-                        fats: _get(Nutrient.fat),
-                        proteins: _get(Nutrient.proteins),
-                        ingredientsText:
-                            product.ingredientsText ?? 'No ingredients listed',
-                      );
-                    }).toList() ??
-                    [];
-              });
-            },
-          ),
-        ),
-      ),
-      body: ListView(
-        children: foods
-            .map(
-              (food) => FoodListTileWidget(
-                food: food,
-                isSelected: false,
-                onSelectedFood: onSelectedFood,
+                          ),
+                        ),
+                        onSubmitted: (String value) async {
+                          print('Searching for: $value');
+                          SearchResult? result = await query.searchProducts(value);
+                          List<Product>? products = result?.products;
+                      
+                          setState(() {
+                            foods =
+                                products?.map((product) {
+                                  final nutr = product.nutriments;
+                      
+                                  // helper to read most nutrients per 100g
+                                  double _get(Nutrient n) =>
+                                      nutr?.getValue(n, PerSize.oneHundredGrams) ??
+                                      0.0;
+                                  // Gotta use helper methods for all the macros since they removed direct getters, e.g. product.carbohydrates
+                                  // Now you have to read through enum values
+                      
+                                  // Calories: prefer kcal; if not available, try kJ -> convert to kcal (1 kcal = 4.184 kJ)
+                                  double _getCalories() {
+                                    final kcal = nutr?.getValue(
+                                      Nutrient.energyKCal,
+                                      PerSize.oneHundredGrams,
+                                    );
+                                    if (kcal != null) return kcal;
+                                    final kj = nutr?.getComputedKJ(
+                                      PerSize.oneHundredGrams,
+                                    );
+                                    if (kj != null) return kj / 4.184;
+                                    return 0.0;
+                                  }
+                      
+                                  return FoodItem(
+                                    brand: product.brands ?? 'Unknown Brand',
+                                    productName:
+                                        product.productName ?? 'Unknown Product',
+                                    calories: _getCalories(),
+                                    carbs: _get(Nutrient.carbohydrates),
+                                    fats: _get(Nutrient.fat),
+                                    proteins: _get(Nutrient.proteins),
+                                    ingredientsText:
+                                        product.ingredientsText ??
+                                        'No ingredients listed',
+                                  );
+                                }).toList() ??
+                                [];
+                          });
+                        },
+                      ),
+                    ),
+                    const Icon(Icons.filter_alt, size: 28,),
+                  ],
+                ),
               ),
-            )
-            .toList(),
+              Padding(
+                padding: const EdgeInsets.only(left: 9.0,),
+                child: Container(
+                  width: 45,
+                  height: 45,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular((16.0)),
+                    color: Colors.white,
+                  ),
+                  child: IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: Icon(Icons.qr_code),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Expanded(
+            child: ListView(
+              children: foods
+                  .map(
+                    (food) => FoodListTileWidget(
+                      food: food,
+                      isSelected: false,
+                      onSelectedFood: onSelectedFood,
+                    ),
+                  )
+                  .toList(),
+            ),
+          ),
+        ],
       ),
       backgroundColor: Theme.of(context).primaryColor,
     );
